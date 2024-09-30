ee21a05b43077f96ffa60c74f1e0327541bb7d54
ipranjal <pranjaltech10@gmail.com>
2024-09-30 12:18:17 +0530

    api

---
+++ microservices/admin-service/src/Controllers/Api.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controllers;
+use App\Helper\Auth;
+
+class Api
+{
+
+    public function getIndex()
+    {
+        $user = Auth::userAPI();
+        if($user == 0){
+            return response()->json(['error' => 'Unauthorized']);
+        }
+
+        $site = db()->find('site')->where('user_id = '.$user)->first();
+        $about = db()->find('about')->where('site_id = '.$site->id)->first();
+
+        return response()->json(['site' => $site->toString(), 'about' => $about->toString()]);
+
+    }
+
+    public function postAuthenticate(){
+        $user = db()->find('user')->where('email = "'.request()->email.'"')->first();
+        if($user){
+            if(password_verify(request()->password, $user->password)){
+                $token = Auth::loginAPI($user->id);
+                return response()->json(['token' => $token]);
+            }
+        }
+        return response()->json(['error' => 'Unauthorized']);
+    }
+
+
+    public function getProjects()
+    {
+        $user = Auth::userAPI();
+        if($user == 0){
+            return response()->json(['error' => 'Unauthorized']);
+        }
+        $site = db()->find('site')->where('user_id = '.$user)->first();
+        $projects = db()->find('project')->where('site_id = '.$site->id)->get();      
+        return response()->json(['site' => $site->toString(),'projects' => $projects->toString()]);
+    }
+
+    public function getContact()
+    {
+       
+        $user = Auth::userAPI();
+        if($user == 0){
+            return response()->json(['error' => 'Unauthorized']);
+        }
+        $site = db()->find('site')->where('user_id = '.$user)->first();
+        $contact = db()->find('contact')->where('site_id = '.$site->id)->first();
+        return response()->json(['site' => $site->toString(), 'contact' => $contact->toString()]);
+    }
+
+
+
+
+
+}
--- microservices/admin-service/src/Helper/Auth.php
+++ microservices/admin-service/src/Helper/Auth.php
@@ -1,24 +1,55 @@
 <?php
 
- namespace App\Helper;
+namespace App\Helper;
+use ReallySimpleJWT\Token;
 
- class Auth{
-    public static function loggedIn(){
+
+class Auth
+{
+    public static function loggedIn()
+    {
         return app()->session()->has('user');
     }
 
-    public static function user(){
+    public static function user()
+    {
         return app()->session()->user;
     }
 
-    public static function logout(){
+    public static function logout()
+    {
         app()->session()->remove('user');
     }
 
-    public static function login($user){
+    public static function login($user)
+    {
         app()->session()->user = $user->id;
         app()->session()->username = $user->username;
     }
-    
-    
- }
+
+
+    public static function loginAPI($user)
+    {
+        $userId = $user->id;
+        $secret = 'AGG@Tgt!+@4YKsb';
+        $expiration = time() + 36000;
+        $issuer = 'iportfolio.me';
+
+        $token = Token::create($userId, $secret, $expiration, $issuer);
+        return $token;
+    }
+    public static function userAPI()
+    {
+        request()->headers->get('Authorization');
+        $token = request()->headers->get('Authorization');
+        $secret = 'AGG@Tgt!+@4YKsb';
+        $valid = Token::validate($token, $secret);
+        if($valid){
+            return Token::getPayload($token)['user_id'];
+        }
+        return 0;
+        
+    }
+
+
+}
